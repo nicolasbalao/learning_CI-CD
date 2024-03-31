fc946d4c34d96910c5d794424dfaab1e55337ae5
nicolas balao <nicolasbalao.pro@gmail.com>
2024-03-31 14:35:55 +0200

    feat(v1): Make the default application for trainning CI/CD

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+type ApiRepsonse = {
+  hello: string;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test';
+  helloWorld$ = this.httpClient.get<ApiRepsonse>('http://localhost:3333/');
+  constructor(private readonly httpClient: HttpClient) {}
 }
