9c761cf792b9e6f1ea5900c7e5d18b275f5b04cf
nicolas balao <nicolasbalao.pro@gmail.com>
2024-03-31 16:06:42 +0200

    feat(api): Change the message for the root api :(

--- backend/app/controllers/hello_worlds_controller.ts
+++ backend/app/controllers/hello_worlds_controller.ts
@@ -2,6 +2,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HelloWorldsController {
   public async index({ response }: HttpContext) {
-    return response.json({ message: 'Hello World' })
+    return response.json({ message: 'Hello you' })
   }
 }
