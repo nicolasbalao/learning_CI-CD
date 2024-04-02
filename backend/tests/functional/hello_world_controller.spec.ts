6047901702a5f3e513998307b0bc6c86965b36a4
nicolas balao <nicolasbalao.pro@gmail.com>
2024-04-02 11:50:52 +0200

    fix test for ci

--- backend/tests/functional/hello_world_controller.spec.ts
+++ backend/tests/functional/hello_world_controller.spec.ts
@@ -4,6 +4,6 @@ test.group('Hello world controller', () => {
   test('example test', async ({ client }) => {
     const response = await client.get('http://localhost:3333/')
     response.assertStatus(200)
-    response.assertBody({ message: 'Hello World' })
+    response.assertBody({ message: 'Hello you' })
   })
 })
